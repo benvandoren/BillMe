f947e07f9f71026f28699a047cf8ee6dd520c00c
Ben VanDoren <bv@benvandoren.com>
2014-07-22 10:19:07 -0400

    first commit, based off of login_app, customer and admin models already made

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.where("auth_token =?", cookies[:auth_token]).first if cookies[:auth_token]
+  end
+
+  # def current_user
+  #   @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+  #   rescue ActiveRecord::RecordNotFound => e
+  #   @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id]) # Use find_by_id to get nil instead of an error if user doesn't exist
+  # end
+
+  #   def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  # helper_method :current_user
+end
---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+	before_save {|user| user.email = email.downcase}
+	before_create { generate_token(:auth_token) }
+	# validates :password, length: {minimum: 6, allow_nil: true}
+	validates :password, length: { minimum: 6 }
+	has_secure_password
+	EMAIL_REGEX = /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
+	validates :email, presence: true, format: {with: EMAIL_REGEX}, uniqueness:{case_sensitive: false}
+
+	def send_password_reset
+		generate_token(:password_reset_token)
+		self.password_reset_sent_at = Time.zone.now
+		save!(validate: false)
+		UserMailer.password_reset(self).deliver
+	end
+
+	private
+
+	def generate_token(column)
+		begin
+			self[column] = SecureRandom.urlsafe_base64
+		end while User.exists?(column => self[column])
+	end
+
+	# def generate_token
+	# 	self.auth_token = SecureRandom.urlsafe_base64
+	# end
+end
